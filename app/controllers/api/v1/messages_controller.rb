24e12e9c5229a5c441f0eeffe833d792751ece2e
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-04-22 17:26:22 +0100

    API index setup completed

---
+++ app/controllers/api/v1/messages_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::MessagesController < ApplicationController
+  before_action :set_channel, only: :index
+
+  def index
+    original_messages = Message.where(channel_id: @param_channel.id)
+    new_messages_array = []
+    original_messages.each do |message|
+      reformatted_message = {
+        id: message.id,
+        author: User.find(message.user_id).email,
+        content: message.content,
+        created_at: message.created_at
+      }
+      new_messages_array << reformatted_message
+    end
+
+    render json: new_messages_array
+  end
+
+  def create
+  end
+
+  private
+
+  def set_channel
+    @param_channel = Channel.find_by(name: params[:channel_id])
+  end
+end
